1a5ad4e7bade0f6e8eecbf16a9651616c0a61b5a
Zhenlin Lian <lianzhenlin@codeaurora.org>
2020-04-16 10:35:00 +0800

    platform/vendor/opensource/display-drivers: add HOTPLUG in function dp for weston
    For weston, enable hotplug in function dp
    Signed-off-by: Zhenlin Lian <lianzhenlin@codeaurora.org>
    
    Change-Id: If97e20f07ed06361ac7ac7954a2c196d7be6108b

--- msm/dp/dp_display.c
+++ msm/dp/dp_display.c
@@ -632,7 +632,8 @@ static void dp_display_send_hpd_event(struct dp_display_private *dp)
 	struct drm_connector *connector;
 	char name[HPD_STRING_SIZE], status[HPD_STRING_SIZE],
 		bpp[HPD_STRING_SIZE], pattern[HPD_STRING_SIZE];
-	char *envp[5];
+	char *envp[6];
+	char *event_string = "HOTPLUG=1";
 
 	if (dp->mst.mst_active) {
 		DP_DEBUG("skip notification for mst mode\n");
@@ -671,7 +672,8 @@ static void dp_display_send_hpd_event(struct dp_display_private *dp)
 	envp[1] = status;
 	envp[2] = bpp;
 	envp[3] = pattern;
-	envp[4] = NULL;
+	envp[4] = event_string;
+	envp[5] = NULL;
 	kobject_uevent_env(&dev->primary->kdev->kobj, KOBJ_CHANGE,
 			envp);
 
