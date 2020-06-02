89d397026f612a5c377bf72fd3d48d650d10bbbb
Abhijit Kulkarni <kabhijit@codeaurora.org>
2020-06-02 16:04:20 -0700

    disp: msm: sde: allow rect1 only configuration
    This change allows the configuration where virtual plane is
    staged without staging the corresponding master plane. Hw allows
    such configuration and additionally during shell stop cases,
    framework removes each staged plane one by one which requires
    this configuration to be supported.
    
    Change-Id: I85ce571dbbc148c069c2cf35cba55ad740d52b5d
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>
    Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

--- msm/sde/sde_crtc.c
+++ msm/sde/sde_crtc.c
@@ -5563,14 +5563,18 @@ static int sde_crtc_atomic_check(struct drm_crtc *crtc,
 
 	for (i = 1; i < SSPP_MAX; i++) {
 		if (pipe_staged[i]) {
-			if (is_sde_plane_virtual(pipe_staged[i]->plane)) {
-				SDE_ERROR(
-					"r1 only virt plane:%d not supported\n",
-					pipe_staged[i]->plane->base.id);
-				rc  = -EINVAL;
-				goto end;
-			}
 			sde_plane_clear_multirect(pipe_staged[i]);
+			if (is_sde_plane_virtual(pipe_staged[i]->plane)) {
+				struct sde_plane_state *psde_state;
+
+				SDE_DEBUG("r1 only virt plane:%d staged\n",
+					 pipe_staged[i]->plane->base.id);
+
+				psde_state = to_sde_plane_state(
+						pipe_staged[i]);
+
+				psde_state->multirect_index = SDE_SSPP_RECT_1;
+			}
 		}
 	}
 
