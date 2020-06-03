7d2d655376be51e74addd8e2a02b889651e3ec54
Jim Wang <jimwan@codeaurora.org>
2020-06-03 12:43:06 -0400

    disp: msm: sde: Remove DRM plane SSPP_LAYOUT property
    -Kernel can internally offset planes that needs
     to be staged on the RIGHT by reducing their
     respective DEST_X coordinate by ½ of the CRTC
     mode width.
    -This will avoid user-space to need to tag each
     planes with SSPP_LAYOUT.
    
    Change-Id: I49d16f2dae672d93d0403842a13840b627beb3dc
    Signed-off-by: Jim Wang <jimwan@codeaurora.org>

--- msm/msm_drv.h
+++ msm/msm_drv.h
@@ -129,7 +129,6 @@ enum msm_mdp_plane_property {
 	PLANE_PROP_SRC_CONFIG,
 	PLANE_PROP_FB_TRANSLATION_MODE,
 	PLANE_PROP_MULTIRECT_MODE,
-	PLANE_PROP_LAYOUT,
 
 	/* total # of properties */
 	PLANE_PROP_COUNT
