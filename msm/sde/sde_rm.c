d25f9d90a58e1d0d0a3b8850264525e2c7d4f789
Jim Wang <jimwan@codeaurora.org>
2020-06-17 17:24:14 -0400

    disp: msm: sde: add support for 3LM topology
    Add 3LM topology variants in resource manager and in drm connector
    topology name property.
    
    Add DSC support for 3-3-3 topology and update encoder related features.
    
    Change-Id: I3609e09b3986c69dfeb299a60aee28dfdab04791
    Signed-off-by: Jim Wang <jimwan@codeaurora.org>

--- msm/sde/sde_rm.c
+++ msm/sde/sde_rm.c
@@ -62,6 +62,8 @@ static const struct sde_rm_topology_def g_ctl_ver_1_top_table[] = {
 	{   SDE_RM_TOPOLOGY_DUALPIPE_3DMERGE_DSC, 2, 1, 1, 1, false },
 	{   SDE_RM_TOPOLOGY_DUALPIPE_DSCMERGE,    2, 2, 1, 1, false },
 	{   SDE_RM_TOPOLOGY_PPSPLIT,              1, 0, 2, 1, false },
+	{   SDE_RM_TOPOLOGY_TRIPLEPIPE,           3, 0, 3, 1, false },
+	{   SDE_RM_TOPOLOGY_TRIPLEPIPE_DSC,       3, 3, 3, 1, false },
 	{   SDE_RM_TOPOLOGY_QUADPIPE_3DMERGE,     4, 0, 2, 1, false },
 	{   SDE_RM_TOPOLOGY_QUADPIPE_3DMERGE_DSC, 4, 3, 2, 1, false },
 	{   SDE_RM_TOPOLOGY_QUADPIPE_DSCMERGE,    4, 4, 2, 1, false },
