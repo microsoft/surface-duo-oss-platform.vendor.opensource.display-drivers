55a5d6eab4a90269e173dab2ab8fa7f79bc01d63
Xiaowen Wu <wxiaowen@codeaurora.org>
2020-07-20 15:28:43 -0400

    disp: msm: increase maximum plane count to 32
    Both shared display and shared plane feature will create planes.
    Increase maximum plane count to 32 to satisfy more scenarios.
    
    Change-Id: I24ccf59352cc32e6bb223f6984dbc60f3caf3568
    Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

--- msm/msm_drv.h
+++ msm/msm_drv.h
@@ -66,7 +66,7 @@ struct msm_gem_vma;
 
 #define NUM_DOMAINS    4    /* one for KMS, then one per gpu core (?) */
 #define MAX_CRTCS      16
-#define MAX_PLANES     20
+#define MAX_PLANES     32
 #define MAX_ENCODERS   16
 #define MAX_BRIDGES    16
 #define MAX_CONNECTORS 16
