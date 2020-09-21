mistersmee/android_kernel_motorola_cebu

disp: msm: dp: add support for test pattern #4
DP specification mandate test pattern #4 for CTS 1.4a. Add
support for the same in link training #2 as per specification.

CRs-Fixed: 2490128
Change-Id: I2f72fec340b56270e7fd1c2940adafe1068bab43
Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

msm/dp/dp_catalog.c
@@ -1212,6 +1212,7 @@ static void dp_catalog_ctrl_set_pattern(struct dp_catalog_ctrl *ctrl,
 {
 	int bit, cnt = 10;
 	u32 data;
+	const u32 link_training_offset = 3;
 	struct dp_catalog_private *catalog;
 	struct dp_io_data *io_data;
 
@@ -1223,17 +1224,28 @@ static void dp_catalog_ctrl_set_pattern(struct dp_catalog_ctrl *ctrl,
 	catalog = dp_catalog_get_priv(ctrl);
 	io_data = catalog->io.dp_link;
 
-	bit = 1;
-	bit <<= (pattern - 1);
-	DP_DEBUG("hw: bit=%d train=%d\n", bit, pattern);
-	dp_write(catalog->exe_mode, io_data, DP_STATE_CTRL, bit);
+	switch (pattern) {
+	case DP_TRAINING_PATTERN_4:
+		bit = 3;
+		break;
+	case DP_TRAINING_PATTERN_3:
+	case DP_TRAINING_PATTERN_2:
+	case DP_TRAINING_PATTERN_1:
+		bit = pattern - 1;
+		break;
+	default:
+		DP_ERR("invalid pattern\n");
+		return;
+	}
 
-	bit = 8;
-	bit <<= (pattern - 1);
+	DP_DEBUG("hw: bit=%d train=%d\n", bit, pattern);
+	dp_write(catalog->exe_mode, io_data, DP_STATE_CTRL, BIT(bit));
+
+	bit += link_training_offset;
 
 	while (cnt--) {
 		data = dp_read(catalog->exe_mode, io_data, DP_MAINLINK_READY);
-		if (data & bit)
+		if (data & BIT(bit))
 			break;
 	}
 

microsoft/surface-duo-oss-platform.vendor.opensource.display-drivers

Change-Id: Idfde6c116a15a32bd89566961c85358af5bdf402
Signed-off-by: Jim Wang <jimwan@codeaurora.org>

msm/dp/dp_catalog.c
@@ -1081,8 +1081,9 @@ static void dp_catalog_panel_config_msa(struct dp_catalog_panel *panel,
 static void dp_catalog_ctrl_set_pattern(struct dp_catalog_ctrl *ctrl,
 					u32 pattern)
 {
-	int bit, cnt = 10;
+	u32 bit, cnt = 10;
 	u32 data;
+	const u32 link_training_offset = 3;
 	struct dp_catalog_private *catalog;
 	struct dp_io_data *io_data;
 
@@ -1094,17 +1095,28 @@ static void dp_catalog_ctrl_set_pattern(struct dp_catalog_ctrl *ctrl,
 	catalog = dp_catalog_get_priv(ctrl);
 	io_data = catalog->io.dp_link;
 
-	bit = 1;
-	bit <<= (pattern - 1);
-	pr_debug("hw: bit=%d train=%d\n", bit, pattern);
-	dp_write(catalog->exe_mode, io_data, DP_STATE_CTRL, bit);
+	switch (pattern) {
+	case DP_TRAINING_PATTERN_4:
+		bit = 3;
+		break;
+	case DP_TRAINING_PATTERN_3:
+	case DP_TRAINING_PATTERN_2:
+	case DP_TRAINING_PATTERN_1:
+		bit = pattern - 1;
+		break;
+	default:
+		pr_err("invalid pattern\n");
+		return;
+	}
 
-	bit = 8;
-	bit <<= (pattern - 1);
+	pr_debug("hw: bit=%d train=%d\n", bit, pattern);
+	dp_write(catalog->exe_mode, io_data, DP_STATE_CTRL, BIT(bit));
+
+	bit += link_training_offset;
 
 	while (cnt--) {
 		data = dp_read(catalog->exe_mode, io_data, DP_MAINLINK_READY);
-		if (data & bit)
+		if (data & BIT(bit))
 			break;
 	}
 